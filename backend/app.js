7fad3bee4536f5b0c2e38d59cf89374d22b6a86b
tebogo-bbd <159124930+tebogo-bbd@users.noreply.github.com>
2024-04-30 22:56:16 +0200

    remove unused error middleware

--- backend/app.js
+++ backend/app.js
@@ -22,13 +22,6 @@ app.use('/api/subjects', subjectsRoutes);
 app.use('/api/attempts', attemptsRoutes);
 app.use('/api/quiz', quizRoutes);
 
-// Error handling middleware, should always be after the routes
-app.use((err, req, res, next) => {
-    res.status(err.statusCode || 500).json({
-        status: err.name || 'error',
-        message: err.message || 'Internal Server Error'
-    });
-});
 sequelize.sync({ force: false }).then(() => {
     logger.info('Database connected and models synchronized');
 }).catch(err => {
