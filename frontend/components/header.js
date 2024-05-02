6d1ae2780741a4757dc33d32f4713186f7706199
Vutlhari Mashimbyi <global-vutlhari.mashimbyi@bbd,ac.za>
2024-05-02 19:09:29 +0200

    Resolved icons path

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -2,10 +2,10 @@ class Header extends HTMLElement{
     connectedCallback(){
         this.innerHTML=`
         <header class="container">
-            <img src="../assets/icons/logo.svg" alt="logo" />
+            <img src="assets/icons/logo.svg" alt="logo" />
   
             <aside>
-                <img src="../assets/icons/moon.svg" alt="theme-toggler" id="mode-toggle-btn" />
+                <img src="assets/icons/moon.svg" alt="theme-toggler" id="mode-toggle-btn" />
                 <h3>Hello, there</h3>
             </aside>
         </header>
