65c8b0d0ecf5f6c286feaac4cb36efdfa27cb836
Siya Nkuna <sudosprogrammer@gmail.com>
2024-05-09 15:05:42 +0200

    SPT-84 detect and save user theme preference

--- frontend/common/utils.js
+++ frontend/common/utils.js
@@ -1,15 +1,28 @@
-export const escapeHTML = (str)=> {
-    if(typeof(str) == 'string'){
+export const escapeHTML = (str) => {
+    if (typeof str == 'string') {
         return str.replace(/[&<>"']/g, function (match) {
             return {
                 '&': '&amp;',
                 '<': '&lt;',
                 '>': '&gt;',
                 '"': '&quot;',
-                "'": '&#39;'
+                "'": '&#39;',
             }[match];
         });
     }
-return "there";
+    return 'there';
+};
 
+export function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function getFromLocalStorage(key) {
+    const item = localStorage.getItem(key);
+
+    return item ? JSON.parse(item) : null;
+}
+
+export function isLocalStorageItemPresent(key) {
+    return localStorage.getItem(key) !== null;
 }
